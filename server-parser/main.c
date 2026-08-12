a0b5c41d1877d821a31a621357353de03ec9df73
lucasbutzke <lucas.butzke@wihex.com.br>
2026-08-12 11:53:27 -0300

    Adding missing variables

--- server-parser/main.c
+++ server-parser/main.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +16,7 @@
 #include "tcp_server.h"
 #include "common.h"
 
+volatile int g_needs_reconfig = 0;
 char g_cmd_line_buf[CMD_LINE_BUF_SIZE];
 size_t g_cmd_line_len = 0;
 uint32_t g_tx_sequence = 0;
@@ -23,6 +25,12 @@ int g_client_fd = -1;
 static void handle_sigint(int sig) {
     (void)sig;
     g_stop = 1;
+}
+
+static long now_ms(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (long)(ts.tv_sec * 1000L + ts.tv_nsec / 1000000L);
 }
 
 
@@ -34,6 +42,7 @@ int main() {
 
     int fd1 = configure_serial_port("/dev/ttyUSB1", B115200);
     int fd2 = configure_serial_port("/dev/ttyUSB0", B921600);
+    const char *file_path  = configFile; // file to send to Port 1
 
     int listen_fd = setup_tcp_listener(TCP_SERVER_PORT);
     if (listen_fd < 0) {
@@ -41,6 +50,19 @@ int main() {
         return 1;
     }
 
+    FILE *file = fopen(file_path, "r");
+    if (!file) {
+        perror("Error opening input file");
+    }
+
+    // Port 1 state
+    port1_state_t state = PORT1_IDLE;
+    char response_buffer[BUFFER_SIZE];
+    size_t response_len = 0;
+    long response_deadline_ms = 0;
+    long cooldown_until_ms = 0; // non-blocking replacement for usleep(100ms)
+
+    // Port 2 state
     // ARM ALIGNMENT FIX: Explicitly direct the compiler to align the static char stream accumulation buffer
     alignas(16) static char port2_accum[PORT2_ACCUM_SIZE];
     size_t port2_accum_len = 0;
@@ -48,6 +70,27 @@ int main() {
     printf("Protocol Engine Server (TCP) running on port %d...\n", TCP_SERVER_PORT);
 
     while (!g_stop) {
+        if (g_needs_reconfig) {
+            printf("\n[RECONFIG] Reiniciando rotina de configuração do radar...\n");
+            
+            tcflush(fd1, TCIOFLUSH);
+            tcflush(fd2, TCIOFLUSH);
+            port2_accum_len = 0;
+            memset(port2_accum, 0, sizeof(port2_accum));
+
+            fseek(file, 0, SEEK_SET);
+
+            // Reseta maquina de estados 
+            state = PORT1_IDLE;
+            cooldown_until_ms = now_ms() + 1000; // aguarda 1s 
+            response_len = 0;
+
+            watchdog_feed(&wdt);
+
+            g_needs_reconfig = 0;
+            printf("[RECONFIG] Iniciando retransmissão do arquivo de configuração...\n");
+        }
+
         struct pollfd fds[4];
         int nfds = 0;
 
